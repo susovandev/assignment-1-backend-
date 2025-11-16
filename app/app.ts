7d1b6e9c33826bdd03560ff58b04315148d264cb
Susovan Dev <susovandas.dev@gmail.com>
2025-11-16 12:27:55 +0530

    Assignment(1): add morgan middleware

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,17 @@
 import express from 'express';
-import type { Application } from 'express';
+import type { Application, Request, Response } from 'express';
 
 const app: Application = express();
 
+// Morgan middleware
+import morgan from './config/morgan.config';
+app.use(morgan);
+
+app.get('/', (req, res) => {
+	return res.status(200).json({
+		statusCode: 200,
+		status: true,
+		message: 'OK',
+	});
+});
 export default app;
